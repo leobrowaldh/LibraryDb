6a58c013b64350cbfa845b736b731902eb5e315a
leobrowaldh <leobrowaldh@gmail.com>
2023-12-06 16:56:49 +0100

    Menu creation started with ConsoleCompanion

--- LibraryDb/Program.cs
+++ LibraryDb/Program.cs
@@ -1,4 +1,6 @@
 ﻿using LibraryDb.Data;
+using ConsoleCompanion;
+using System.Runtime.InteropServices;
 
 namespace LibraryDb
 {
@@ -6,12 +8,61 @@ namespace LibraryDb
     {
         static void Main(string[] args)
         {
-            #region Inicial seeding
             DataAccess dataAccess = new DataAccess();
+            RunMenu(dataAccess);
+        }
+
+        private static void RunMenu(DataAccess dataAccess)
+        {
+            Console.WriteLine("\n------Welcome to your Local Amazing Library!------\n");
+            ConsoleCompanionHelper cc = new();
+            int selectedOption = -1;
+            var menuSelections = new List<string> {"Seed the library with some initial Data", "Create an author", "Create a book", 
+            "Create a new customer", "Borrow a book", "Return a book", "Delete a customer", "Delete books", "Delete an author"};
+            string menuGuideText = "Menu options, select with enter, ESC to Exit";
+            do
+            {
+                selectedOption = cc.CreateMenu(menuSelections, menuGuideText, ConsoleColor.Green, true);
+
+                switch (selectedOption)
+                {
+                    case 0:
+                        {
+                            SeedLibrary(dataAccess);
+                            break;
+                        }
+                    case 1:
+                        {
+                            
+                            break;
+                        }
+                    case 2:
+                        {
+                            
+                            break;
+                        }
+                    case 3:
+                        {
+                            
+                            break;
+                        }
+                }
+            }
+            while (selectedOption != -1);
+        }
+
+        private static void SeedLibrary(DataAccess dataAccess)
+        {
             dataAccess.SeedLibrary(20, 2, 10);
             dataAccess.SeedCustomers(10);
-            #endregion
+            PrintInColor(ConsoleColor.Green, "Library seeded. Can be seeded again if you want more data.");
+        }
 
+        private static void PrintInColor(ConsoleColor color, string toPrint)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(toPrint);
+            Console.ResetColor();
         }
     }
 }
