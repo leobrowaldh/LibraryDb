40c0cc1b4c2b6f12e151cd162f74df6e8bbb99aa
leobrowaldh <leobrowaldh@gmail.com>
2023-12-07 19:38:04 +0100

    case 0 to 4 ready in the menu

--- LibraryDb/Program.cs
+++ LibraryDb/Program.cs
@@ -1,6 +1,8 @@
 ﻿using LibraryDb.Data;
 using ConsoleCompanion;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Linq;
 
 namespace LibraryDb
 {
@@ -43,32 +45,77 @@ namespace LibraryDb
                         }
                     case 2:
                         {
-                            Console.Clear();
-                            string title = cc.AskForString("Book Title: ");
-                            int year = cc.AskForInt("Year: ");
-                            int rating = cc.AskForInt("Rating (1 - 5): ");
-                            if (rating < 1 ||  rating > 5)
-                            {
-                                Feedback("Rating has to be between 1 and 5");
-                            }
-                            List<int>authorIds = new List<int>();
-                            int numberOfAuthors = cc.AskForInt("How many authors do your book have? ");
-                            for (int i = 0; i < numberOfAuthors; i++)
-                            {
-                                int authorId = cc.AskForInt($"Author ID: ");
-                                authorIds.Add(authorId);
-                            }
-                            int numberOfCopies = cc.AskForInt("Number of copies of the book: ");
-
-                            int IsbnId = dataAccess.CreateNewIsbn(title, year, rating, authorIds, out _);
-                            int bookId = dataAccess.CreateCopyOfExistingIsbn(IsbnId, out _);
-
-
+                            int IsbnId = CreateNewBook(dataAccess, cc);
+                            Feedback($"--- New book created with IsbnId = {IsbnId} ---");
                             break;
                         }
                     case 3:
                         {
-                            Console.Clear();
+                            List<int> booksIds = BookCopiesCreate(dataAccess, cc);
+                            string bookIdString = string.Join(", ", booksIds);
+                            Feedback($"--- Book copies created with Id: {bookIdString} ---");
+                            break;
+                        }
+                    case 4:
+                        {
+                            string firstName = cc.AskForString("Customer first name: ");
+                            string lastName = cc.AskForString("Customer last name: ");
+                            dataAccess.CreateNewCustomer(firstName, lastName);
+                            Feedback($"--- Customer {firstName} {lastName} created, Personal Library Card created ---");
+                            break;
+                        }
+                    case 5:
+                        {
+                            //borrow a book
+
+                            break;
+                        }
+                    case 6:
+                        {
+                            //Return a book
+
+                            break;
+                        }
+                    case 7:
+                        {
+                            //Delete a customer
+
+                            break;
+                        }
+                    case 8:
+                        {
+                            //Delete books
+
+                            break;
+                        }
+                    case 9:
+                        {
+                            //Delete an author
+
+                            break;
+                        }
+                    case 10:
+                        {
+                            //Show customer orderhistory
+
+                            break;
+                        }
+                    case 11:
+                        {
+                            //Show book orderhistory
+
+                            break;
+                        }
+                    case 12:
+                        {
+                            //Show all books in librar
+
+                            break;
+                        }
+                    case 13:
+                        {
+                            //Show all customers
+
                             break;
                         }
                 }
@@ -76,9 +123,49 @@ namespace LibraryDb
             while (selectedOption != -1);
         }
 
+        private static List<int> BookCopiesCreate(DataAccess dataAccess, ConsoleCompanionHelper cc)
+        {
+            int isbnId = cc.AskForInt("IsbnId of book: ", "Please enter a valid IsdnID");
+            int numberOfCopies = cc.AskForInt("Number of copies to add: ", "Invalid input");
+            List<int> booksIds = CreateCopiesOfIsbn(dataAccess, numberOfCopies, isbnId);
+            return booksIds;
+        }
+
+        private static int CreateNewBook(DataAccess dataAccess, ConsoleCompanionHelper cc)
+        {
+            string title = cc.AskForString("Book Title: ");
+            int year = cc.AskForInt("Year: ");
+            int rating = cc.AskForInt("Rating (1 - 5): ");
+            if (rating < 1 || rating > 5)
+            {
+                Feedback("Rating has to be between 1 and 5");
+            }
+            List<int> authorIds = new List<int>();
+            int numberOfAuthors = cc.AskForInt("How many authors do your book have? ");
+            for (int i = 0; i < numberOfAuthors; i++)
+            {
+                int authorId = cc.AskForInt($"Author ID: ");
+                authorIds.Add(authorId);
+            }
+            int numberOfCopies = cc.AskForInt("Number of copies of the book: ");
+            int IsbnId = dataAccess.CreateNewIsbn(title, year, rating, authorIds, out _);
+            CreateCopiesOfIsbn(dataAccess, numberOfCopies, IsbnId);
+            return IsbnId;
+        }
+
+        private static List<int> CreateCopiesOfIsbn(DataAccess dataAccess, int numberOfCopies, int IsbnId)
+        {
+            List<int> bookIds = new List<int>();
+            for (int i = 0; i < numberOfCopies; i++)
+            {
+                int bookId = dataAccess.CreateCopyOfExistingIsbn(IsbnId, out _);
+                bookIds.Add(bookId);
+            }
+            return bookIds;
+        }
+
         private static int AuthorCreate(ConsoleCompanionHelper cc, DataAccess dataAccess)
         {
-            Console.Clear();
             string authorName = cc.AskForString("Enter the name of the new author ");
             int authorId = dataAccess.CreateAuthor(authorName, out _);
             return authorId;
